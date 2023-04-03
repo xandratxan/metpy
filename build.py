312e3202d2b793a278113facf9d042110f3a6eed
Xandra Campo <xkmpera@gmail.com>
2023-04-03 11:02:51 +0200

    Refactor build functions

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 """Script to build and update documentation."""
-from distutils.dir_util import copy_tree
 
 import src.definitions as df
+import src.functions as fnc
 from src.html.pages.documentation import metpy_documentation
 from src.html.pages.index import metpy_index
 from src.html.pages.packages import metpy_packages, metpy_package_magnitude
@@ -36,63 +36,18 @@ def metpy_pages(update):
         f.close()
 
 
-def build_readme(package, text, update):
-    """Build and update the package README.md."""
-    destination = 'public' if update else 'private'
-    file = df.readme_paths[destination][package]
-    f = open(file, "w")
-    f.write(text)
-    f.close()
-
-
-def build_rst_files(package, rst_texts):
-    """Build the package RST files for Sphinx."""
-    rst_files = [
-        df.rst_paths[package]['index'],
-        df.rst_paths[package]['tutorial'],
-        df.rst_paths[package]['howto'],
-        df.rst_paths[package]['api'],
-    ]
-    for text, file in zip(rst_texts, rst_files):
-        f = open(file, "w")
-        f.write(text)
-        f.close()
-
-
-def export_rst_files(package):
-    """Copy the package RST files for Sphinx to the destination repository."""
-    src = df.sphinx_paths[package]['local']['rst']
-    dst = df.sphinx_paths[package]['remote']['rst']
-    copy_tree(src=src, dst=dst)
-
-
-def import_html_files(package):
-    """Copy the package HTML files of Sphinx from the destination repository."""
-    src = df.sphinx_paths[package]['remote']['html']
-    dst = df.sphinx_paths[package]['local']['html']
-    copy_tree(src=src, dst=dst)
-
-
-def build_package_docs(package, rst_texts, update_readme, export_rst, import_html):
-    """Build the documentation of a package."""
-    build_readme(package=package, text=package_magnitude_readme, update=update_readme)
-    build_rst_files(package=package, rst_texts=rst_texts)
-    if export_rst:
-        export_rst_files(package=package)
-    if import_html:
-        import_html_files(package=package)
-
-
 def build_magnitude_docs(update_readme, export_rst, import_html):
     """Build the documentation of the physical-magnitude package."""
     package = 'magnitude'
+    readme_text = package_magnitude_readme
     rst_texts = [
         package_magnitude_index,
         package_magnitude_tutorial,
         package_magnitude_howto,
         package_magnitude_api,
     ]
-    build_package_docs(package, rst_texts, update_readme, export_rst, import_html)
+    fnc.build_package_docs(package=package, readme_text=readme_text, rst_texts=rst_texts,
+                           update_readme=update_readme, export_rst=export_rst, import_html=import_html)
 
 
 if __name__ == '__main__':
--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,6 @@
 """Functions to build and update documentation."""
+import src.definitions as df
+from distutils.dir_util import copy_tree
 
 
 def html_resources(parent=False, grandparent=False):
@@ -33,3 +35,50 @@ def install_from_clone(package_name, author_github_username):
 def package_link(author_github, name, version):
     """Template of installation link from server."""
     return f'git+{author_github}{name}#egg={name}-{version}" data-requires-python="&gt;=3.6.0'
+
+
+def build_readme(package, text, update):
+    """Build and update the package README.md."""
+    destination = 'public' if update else 'private'
+    file = df.readme_paths[destination][package]
+    f = open(file, "w")
+    f.write(text)
+    f.close()
+
+
+def build_rst_files(package, rst_texts):
+    """Build the package RST files for Sphinx."""
+    rst_files = [
+        df.rst_paths[package]['index'],
+        df.rst_paths[package]['tutorial'],
+        df.rst_paths[package]['howto'],
+        df.rst_paths[package]['api'],
+    ]
+    for text, file in zip(rst_texts, rst_files):
+        f = open(file, "w")
+        f.write(text)
+        f.close()
+
+
+def export_rst_files(package):
+    """Copy the package RST files for Sphinx to the destination repository."""
+    src = df.sphinx_paths[package]['local']['rst']
+    dst = df.sphinx_paths[package]['remote']['rst']
+    copy_tree(src=src, dst=dst)
+
+
+def import_html_files(package):
+    """Copy the package HTML files of Sphinx from the destination repository."""
+    src = df.sphinx_paths[package]['remote']['html']
+    dst = df.sphinx_paths[package]['local']['html']
+    copy_tree(src=src, dst=dst)
+
+
+def build_package_docs(package, readme_text, rst_texts, update_readme, export_rst, import_html):
+    """Build the documentation of a package."""
+    build_readme(package=package, text=readme_text, update=update_readme)
+    build_rst_files(package=package, rst_texts=rst_texts)
+    if export_rst:
+        export_rst_files(package=package)
+    if import_html:
+        import_html_files(package=package)
